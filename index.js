4e0f4e773363f92b677d8cea11348eb382d4e69e
bayuubay <bayu.suryo.a@gmail.com>
2021-05-01 11:46:32 +0700

    fix socket cors

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 // const io = socketio(server);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "https://chat-apps-client.herokuapp.com/",
+    origin: "https://chat-apps-client.herokuapp.com",
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true,
