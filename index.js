e5dbbcb504647abd96d506add406e0c01671b5c0
bayuubay <bayu.suryo.a@gmail.com>
2021-05-02 10:44:53 +0700

    fix bug client

--- index.js
+++ index.js
@@ -71,6 +71,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(process.env.PORT || 8000, () =>
+server.listen(process.env.PORT || 5000, () =>
   console.log(`Server has started.`)
 );
